f33e0eb7e4c3a3af044f3e7d89029dca5a3028a3
Alvin Lumowa <wtflol@gmail.com>
2022-07-29 16:23:04 +0200

    Add Util class

---
+++ modules/Util.js
@@ -0,0 +1,11 @@
+class Util {
+
+    static DaysSinceBoot(bootTime) {
+
+
+        //Returning minutes for now
+        return Math.round((Date.now() - bootTime) / (60 * 1000));
+    }
+}
+
+exports = Util;
