6e4da36d17ea0d6d30a5188724c338271dce74d6
Alvin Lumowa <wtflol@gmail.com>
2022-07-30 15:07:17 +0200

    Integrate module changes to main logic

--- projects/index.js
+++ projects/index.js
@@ -1,50 +1,73 @@
-const EventGenerator = require('EventGenerator');
-const Util = require('Util');
-const Display = require('Display');
+const EventGenerator = require("EventGenerator");
+const Display = require("Display");
+const Storage = require("Storage");
+const Network = require("Network");
 
-//Data imports
-const images = require('images');
-const randomText = require('data_random');
-
-const TickRateMS = 500;
+const TickRateMS = 5 * 1000;
 const BootTime = Date.now();
 
-//Called when display is ready
-function onDisplayReady(){
-
-  // console.log('display connected: ' + Date.now());
-  // kristina.startHearts();
-}
-
+E.setTimeZone(2);
 //Init i2c buses
 I2C1.setup({ scl: D32, sda: D33 });
+I2C2.setup({ scl: D26, sda: D27 });
 
 //Initialize displays
-const kristina = new Display(I2C1, 0x3C);
-kristina.on('ready', onDisplayReady);
+const kristina = new Display(I2C1, 0x3c);
 kristina.connect();
 
+const phillip = new Display(I2C2, 0x3c);
+phillip.connect();
 
+const credentials = Storage.readJSON("wifi_credentials.json");
+const network = new Network(credentials.ssid, credentials.password);
+network.start();
+
+function renderDaily() {
+  //Calculate days to wedding and pull fetch matching data
+  const weddingTime = 1664488800000;
+  const daysUntil = Math.ceil((weddingTime - Date.now()) / (86400 * 1000));
+  let prefix;
+  if (daysUntil >= 50) {
+    prefix = 1;
+  } else {
+    prefix = 51 - daysUntil;
+  }
+
+  const data = Storage.readJSON(`${prefix}.json`);
+  kristina.write(data.k);
+  phillip.write(data.p);
+}
+
+function renderRandom() {
+  const randomText = Storage.readJSON(`random.json`);
+  kristina.write(randomText[Math.floor(Math.random() * randomText.length)]);
+  phillip.write(randomText[Math.floor(Math.random() * randomText.length)]);
+}
 
 //Initialize clock & configure event probabilities
 const events = new EventGenerator(TickRateMS);
-events.on('tick', (event) => {
-
+events.on("tick", (event) => {
   // console.log(`Minutes since boot ${EventGenerator.DaysSinceBoot(BootTime)}`);
+  const p = process.memory(false);
+  const now = new Date();
+  console.log(`${now.toString()}: ${JSON.stringify(p)}`);
 
-  switch(event){
+  switch (event) {
     case events.TYPES.DAILY:
-      kristina.write('Show daily message');
+      renderDaily();
       break;
+
     case events.TYPES.RANDOM:
-      const index = Math.floor((Math.random() * randomText.length));
-      kristina.write(randomText[index].k);
+      renderRandom();
       break;
+
     case events.TYPES.HEARTS:
-      kristina.write('Show hearts');
+      [kristina, phillip].forEach((display) => display.startHearts());
       break;
+
     case events.TYPES.QR:
-      kristina.write('Show QR');
+      kristina.write("Show QR");
+      phillip.write("Show QR");
       break;
   }
 });
